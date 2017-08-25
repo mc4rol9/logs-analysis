6de9fd9323a9a72de89331e331c1ed44cfbc40d5
mc9 <mariacarol9@gmail.com>
2017-08-25 17:49:41 -0300

    Make SQL script for views

---
+++ sql_views.sql
@@ -0,0 +1,44 @@
+-- THE SQL VIEWS --
+
+-- View for question 1 --
+
+CREATE VIEW top_articles AS
+  SELECT title, 
+  COUNT(log.id) AS pageviews
+  FROM articles, log
+    WHERE log.path = CONCAT('/article/', articles.slug)
+  GROUP BY articles.title 
+  ORDER BY pageviews desc;
+
+
+-- View for question 2 --
+
+CREATE VIEW top_authors AS
+  SELECT title, name
+  FROM articles, authors
+    WHERE articles.author = authors.id;
+
+
+-- Views for question 3 --
+
+CREATE VIEW log_total AS
+  SELECT date(time) as days, 
+  CAST(count(status) AS FLOAT) AS status
+  FROM log
+  GROUP BY days
+  ORDER BY days;
+
+CREATE VIEW log_errors AS
+  SELECT date(time) as days, 
+  CAST(count(status) AS FLOAT) AS errors
+  FROM log
+    WHERE status!='200 OK'
+  GROUP BY days
+  ORDER BY days;
+
+CREATE VIEW log_percentage AS
+  SELECT log_errors.days, 
+  CAST((log_errors.errors*100)/log_total.status AS FLOAT) AS percentage
+  FROM log_errors, log_total
+    WHERE log_errors.days = log_total.days
+  ORDER BY log_errors.days;
