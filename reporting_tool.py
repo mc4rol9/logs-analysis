879e043d5d5cdef45ea04435e191e2f0319133f3
mc9 <mariacarol9@gmail.com>
2017-08-25 17:58:05 -0300

    Code correction to Python 3

--- reporting_tool.py
+++ reporting_tool.py
@@ -18,20 +18,20 @@ def db_execute(query):
         db.close()
         return answer
     except:
-        print "\nSorry, unable to connect to the database: " + DBNAME
+        print("\nSorry, unable to connect to the database: " + DBNAME)
 
 
 def question_one():
     """Pass query to execution. Print out the result to question 1."""
     query = ("select * from top_articles limit 3")
     answer = db_execute(query)
-    print "\n QUESTION 1: What are the most popular "\
-          "three articles of all time?\n"
-    print " ANSWER:"
-    print "\n  -- THE TOP 3 Popular Articles --\n"
+    print("\n QUESTION 1: What are the most popular "\
+          "three articles of all time?\n")
+    print(" ANSWER:")
+    print("\n  -- THE TOP 3 Popular Articles --\n")
     count = 1
     for a in answer:
-        print " ", count, a[0], " with", a[1], "views\n"
+        print(" ", count, a[0], " with", a[1], "views\n")
         count = count + 1
 
 
@@ -41,13 +41,13 @@ def question_two():
              "from top_authors, top_articles group by name "
              "order by views desc")
     answer = db_execute(query)
-    print "\n QUESTION 2: Who are the most popular "\
-          "article authors of all time?\n"
-    print " ANSWER:"
-    print "\n  -- MOST Popular Authors --\n"
+    print("\n QUESTION 2: Who are the most popular "\
+          "article authors of all time?\n")
+    print(" ANSWER:")
+    print("\n  -- MOST Popular Authors --\n")
     count = 1
     for a in answer:
-        print " ", count, a[0], " with", a[1], "views\n"
+        print(" ", count, a[0], " with", a[1], "views\n")
         count = count + 1
 
 
@@ -56,14 +56,14 @@ def question_three():
     query = ("select * from log_percentage "
              "where percentage > 1.0 order by days")
     answer = db_execute(query)
-    print "\n QUESTION 3: On which days did more than 1%"\
-          "of requests lead to errors?\n"
-    print " ANSWER:"
-    print "\n  -- DAYS/ERRORS LIST --\n"
+    print("\n QUESTION 3: On which days did more than 1%"\
+          "of requests lead to errors?\n")
+    print(" ANSWER:")
+    print("\n  -- DAYS/ERRORS LIST --\n")
     count = 1
     for a in answer:
-        print " ", count, a[0], "with a percentage of", ("%.2f" % a[1]),\
-              "errors\n"
+        print(" ", count, a[0], "with a percentage of", ("%.2f" % a[1]),\
+              "errors\n")
         count = count + 1
 
 
